357490c65fea3ee96d8c8368091ed9954b1ec033
Velichko Alexander <sasha0992@gmail.com>
2025-11-17 01:37:07 +0400

    fix: resolve 28 task

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -513,8 +513,9 @@ function getMaxItems(arr, n) {
  *    findCommonElements(['a', 'b', 'c'], ['b', 'c', 'd']) => [ 'b', 'c' ]
  *    findCommonElements([1, 2, 3], ['a', 'b', 'c']) => []
  */
-function findCommonElements(/* arr1, arr2 */) {
-  throw new Error('Not implemented');
+function findCommonElements(arr1, arr2) {
+  const comEL = arr1.filter((element) => arr2.includes(element));
+  return comEL;
 }
 
 /**
