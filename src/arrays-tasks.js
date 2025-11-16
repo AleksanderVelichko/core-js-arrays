d2221bd2703c4cbc23fb0e143cdbbc369440d060
Velichko Alexander <sasha0992@gmail.com>
2025-11-16 22:43:09 +0400

    fix: resolve 8 task

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -152,8 +152,9 @@ function getAverage(arr) {
  *    isSameLength(['orange', 'banana', 'cherry']) => true
  *    isSameLength(['cat', 'dog', 'elephant']) => false
  */
-function isSameLength(/* arr */) {
-  throw new Error('Not implemented');
+function isSameLength(arr) {
+  const referLength = arr[0].length;
+  return arr.every((str) => str.length === referLength);
 }
 
 /**
