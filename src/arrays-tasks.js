f956a9f937a674f010fde6ae0caab3fb38f3b3e3
Velichko Alexander <sasha0992@gmail.com>
2025-11-17 01:13:09 +0400

    fix: resolve 25 task

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -457,8 +457,12 @@ function getIdentityMatrix(n) {
  *    getIndicesOfOddNumbers([2, 4, 6, 8, 10]) => []
  *    getIndicesOfOddNumbers([11, 22, 33, 44, 55]) => [0, 2, 4]
  */
-function getIndicesOfOddNumbers(/* numbers */) {
-  throw new Error('Not implemented');
+function getIndicesOfOddNumbers(numbers) {
+  const odd = numbers
+    .map((value, index) => (value % 2 !== 0 ? index : undefined))
+    .filter((index) => index !== undefined);
+
+  return odd;
 }
 
 /**
