74751a1e370564f77883c25cec6bd45e8c4ac8fb
Velichko Alexander <sasha0992@gmail.com>
2025-11-16 22:38:07 +0400

    fix: resolve 7 task

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -131,8 +131,15 @@ function getStringsLength(arr) {
  *   getAverage([ 1, 10, 100, 1000 ])  => 277,75
  *   getAverage([ 2, 3, 3 ])  => 2,67
  */
-function getAverage(/* arr */) {
-  throw new Error('Not implemented');
+function getAverage(arr) {
+  if (arr.length === 0) {
+    return 0;
+  }
+  const sum = arr.reduce((acc, value) => acc + value, 0);
+
+  const average = sum / arr.length;
+  const roundAverage = Math.round(average * 100) / 100;
+  return roundAverage;
 }
 
 /**
